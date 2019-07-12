dfaf75da2277d3b3c8dbb55af52079222082f130
LRUser <lruser@LRUsers-MacBook-Pro.local>
2019-07-12 09:57:39 +0800

    erye

---
+++ Documents/demo/src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @RequestMapping(value="/demo",method= RequestMethod.GET)
+
+    public String demo(){
+        return "hello world";
+    }
+}
